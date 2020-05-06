a1e70f4b8d8eb987d6a2296c4437a9df554d0f8d
Your Name <you@example.com>
2020-05-06 08:15:47 +0000

    Clean Up commented Code

--- src/functionHandlers/verify/verify.ts
+++ src/functionHandlers/verify/verify.ts
@@ -1,10 +1,10 @@
 import { APIGatewayEvent } from "aws-lambda";
+import { verify, isValid } from "@govtechsg/oa-verify";
 import { publicRequestHandler } from "../../middlewares/handlers";
-import { verify, isValid } from "@govtechsg/opencerts-verify";
 import { config } from "../../config";
 
 const handleVerify = async (event: APIGatewayEvent) => {
-    const { document } = JSON.parse(event.body ?? "");
+    const document = JSON.parse(event.body ?? "");
     const fragments = await verify(document, { network: config.network });
     const results = {
         summary: {
@@ -17,54 +17,6 @@ const handleVerify = async (event: APIGatewayEvent) => {
       }
     
     return results;
-};
+}
 
 export const handler = publicRequestHandler(handleVerify);
-
-// import { APIGatewayEvent } from "aws-lambda";
-// import uuid from "uuid/v4";
-// import createHttpError from "http-errors";
-// import { publicRequestHandler } from "../../middlewares/handlers";
-// import { getValidUserByOperatorToken, putAuthorisation, User } from "../../models/user";
-// import { getLogger } from "../../common/logger";
-// import { config } from "../../config";
-
-// const { info } = getLogger("otpConfirmation");
-
-// export const validateOtp = async (user: User, otp: string) => {
-//   // If OTP does not match, we will increment the otp
-//   const currentRetryCount = (user.otpRetryCount || 0) + 1;
-//   if (currentRetryCount > config.appParameters.maxOtpRetries)
-//     throw new createHttpError.BadRequest("maximum number of otp retries reached");
-
-//   if (otp === user.otp && user.otp) return;
-
-//   await putAuthorisation({ ...user, otpRetryCount: currentRetryCount });
-//   throw new createHttpError.BadRequest("otp does not match");
-// };
-
-// export const handleConfirm = async (event: APIGatewayEvent) => {
-//   const { code, phone, otp } = JSON.parse(event.body ?? "");
-//   if (!code || typeof code !== "string") throw new createHttpError.BadRequest("code should be a string");
-//   const user = await getValidUserByOperatorToken(codevent.body ?? ""e);
-//   if (phone !== user.phone && user.phone) throw new createHttpError.BadRequest("phone does not match");
-//   if (user.sessionToken) throw new createHttpError.BadRequest("sessionToken has not been invalidated");
-//   await validateOtp(user, otp);
-
-//   // Generate session and save
-//   const sessionToken = uuid();
-//   await putAuthorisation({ ...user, sessionToken, otp: undefined, otpRetryCount: undefined });
-//   info(
-//     `${JSON.stringify({
-//       code: "SESSION_CREATED",
-//       phone,
-//       operatorToken: user.operatorToken,
-//       userReference: user.userReference,
-//       sourceIp: event?.requestContext?.identity?.sourceIp
-//     })}`
-//   );
-
-//   return { sessionToken, ttl: user.validTill };
-// };
-
-// export const handler = publicRequestHandler(handleConfirm);
--- src/middlewares/handlers/index.ts
+++ src/middlewares/handlers/index.ts
@@ -4,8 +4,11 @@ import httpSecurityHeaders from "@middy/http-security-headers";
 import JSONErrorHandlerMiddleware from "middy-middleware-json-error-handler";
 import { withBoundary } from "../withBoundary";
 
-export const publicRequestHandler = (handler: any) =>
-  middy(handler)
+export const publicRequestHandler = (handler: any) => {
+  return middy(handler)
     .use(cors())
     .use(httpSecurityHeaders())
+    .use(JSONErrorHandlerMiddleware())
     .use(withBoundary());
+}
+  
